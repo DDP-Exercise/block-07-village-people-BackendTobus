18cd6a9d1eee0934c2c8433b1e03c6f11d8f71fa
Tobi <tobias2004.h@gmail.com>
2026-06-10 13:48:53 +0200

    VillagePeople_Hofinger

--- js/class.citizen.js
+++ js/class.citizen.js
@@ -11,10 +11,18 @@
  *
  */
 
-export default class Citizen{
+export default class citizen{
 
     /* Math.random() gives a value from 0 to <1
     3 scales it to 0–<3
     Math.floor(...) makes it 0, 1, 2
     + 2 shifts it to 2, 3, 4 */
+    constructor(name){
+        this.name = name;
+        this.rank = Math.floor(Math.random() * 3) + 2;
+        this.home = null;
+    }
+    toString(){
+        return `${this.name}, Citizen, Rank ${this.rank} `;
+    }
 }
--- js/class.nobleBuilding.js
+++ js/class.nobleBuilding.js
@@ -1,6 +1,23 @@
 "use strict";
 
+import building from "./class.building.js";
+
 /**
  * Create a NobleBuilding class. It's basically the same as a regular building
  * with one exception: Only nobles allowed.
  */
+
+export default class NobleBuilding extends building{
+    constructor(name, capacity, residents) {
+        super(name, capacity, residents);
+    }
+    addResident(citizen, resident) {
+        if(citizen.rank == 1) {
+            if (this.residents.length < this.capacity) {
+                this.residents.push(citizen)
+                citizen.home = this.name
+                return true;
+            } else return this.makeSpaceFor(citizen)
+        }
+    }
+}
